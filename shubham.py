4fea4ddc20ef1a39538250f92965019db33b5cfb
shubhammankar3 <146057564+shubhammankar3@users.noreply.github.com>
2023-09-26 11:07:21 +0530

    Create shubham.py

---
+++ shubham.py
@@ -0,0 +1,6 @@
+def add(a,b):
+  return a+b
+  num1=int(input("enter the first num:"))
+  num2=int(input("enter the num"))
+  sum_of_twonumbers=add(num1,num1)
+  print("sum of {0} and {1} is {2} :".format(num1,num2,sum_of_twonumbers))
